a3b5a2f63472fee072eaf0ea8fd4abae94cc7070
Miraculix75 <88479889+Miraculix75@users.noreply.github.com>
2024-11-07 19:55:43 +0100

    Updated HTML and CSS code. The sidebare is working as expected and the button worked as expected. Perhaps further working on the sidbar-nav on colapsing fo ul and li.

---
+++ src/script/dropdown-btn.js
@@ -0,0 +1,13 @@
+
+document.querySelectorAll('.dropdown-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('open');
+        const subMenu = button.nextElementSibling;
+        if (subMenu.style.maxHeight) {
+            subMenu.style.maxHeight = null;
+        } else {
+            subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+        }
+    });
+});
+
